a9157560a21fcdb4184ae9d2fb0c5a6195a9a234
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-07-11 04:23:56 +0530

    add parallax effect

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 (function(){
-	
 	//Variables
-	var documentElem = $(document),
-		projectVisibleContent = $('.project-visible-content'),
-		nav = $('nav');
-		navigateElems = $('nav li a, .continue-btn'),
-		htmlBody = $('html','body');
-		lastScrollTop = 0;
-
+    var documentElem = $(document),
+        projectVisibleContent = $('.project-visible-content'),
+        projectExtraToggleBtn = projectVisibleContent.find('.project-extra-toggle'),
+        nav = $('nav'),
+        navigateElems = $('nav li a, .continue-btn'),
+        htmlBody = $('html, body'),
+        introBg = $('.intro');
+        lastScrollTop = 0;
 		//Nav toggle
 		documentElem.on('scroll', function(){
 			var currentScrollTop = $(this).scrollTop();
@@ -21,11 +21,20 @@
 			}
 			lastScrollTop = currentScrollTop;
 		});
+        
+         navigateElems.on('click', function(e) {
+        var targetElem = $( $(this).data('scroll-target') ),
+            targetOffsetTop = targetElem.offset().top;
+        
+        htmlBody.animate({
+            scrollTop: targetOffsetTop
+        }, 400);
+        e.preventDefault();
+    	});
 
-		navigateElems.on('click', function(e){
-			var targetElem = $($(this).data('scroll-target'));
-			console.log(targetElem);
-
-			e.preventDefault();
-		});
+          documentElem.on('scroll',function(){
+            var currentScrollTop = $(this).scrollTop();
+           introBg.css('background-position', '50%' + -currentScrollTop/4 + 'px');
+           e.preventDefault();
+        });
 })();
