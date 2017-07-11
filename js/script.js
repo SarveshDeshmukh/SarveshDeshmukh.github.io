948351d2cd9fa16b16c33b6d66fb94231b3644a2
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-07-11 16:44:06 +0530

    add animation code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+$(window).load(function(){
+
+	var current = $(window).scrollTop();
+	$(window).scroll(function() {
+		$(window).scrollTop(current);
+	});
+	$('.section').delay(10000).fadeOut(15000);
+
+	$('#preloader').delay(10000).fadeOut(5000,function(){
+		$('nav').css('opacity',1);
+		$('.social-head li').css('opacity',1);
+		$(window).off('scroll');
+
+	});  	 	
+
+});
+
 (function(){
-	//Variables
-    var documentElem = $(document),
+				//Variables
+	    var documentElem = $(document),
         projectVisibleContent = $('.project-visible-content'),
         projectExtraToggleBtn = projectVisibleContent.find('.project-extra-toggle'),
         nav = $('nav'),
@@ -8,6 +25,7 @@
         htmlBody = $('html, body'),
         introBg = $('.intro');
         lastScrollTop = 0;
+
 		//Nav toggle
 		documentElem.on('scroll', function(){
 			var currentScrollTop = $(this).scrollTop();
